9ef4d89624d9faadaec6b5d1327469fc5c59a03f
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-04-25 10:51:36 -0700

    add inventory and classType components
    Co-authored-by: Gabe Nielsen <NielsenG62@users.noreply.github.com>
    Co-authored-by: Emilio <EmilioG1@users.noreply.github.com>
    Co-authored-by: m-wright92 <m-wright92@users.noreply.github.com>

--- src/js/Character.js
+++ src/js/Character.js
@@ -4,10 +4,11 @@ class Character {
     this.atk = attack;
     this.eva = evade;
     this.def = defense;
-
+    this.lvl = 1;
     this.maxHp = hp;
+    this.money = 0;
     this.inventory = [];
-
+    this.classType = {};
   }
 
   class(classType) {
@@ -16,16 +17,46 @@ class Character {
       this.atk = 75;
       this.eva = 50;
       this.def = 40;
+      this.maxHp = this.hp;
+      this.classType = "Fighter";
     }else if (classType === "Rogue") {
       this.hp = 70;
       this.atk = 65;
       this.eva = 80;
       this.def = 30;
+      this.maxHp = this.hp;
+      this.classType = "Rogue";
     }else {
       this.hp = 90;
       this.atk = 50;
       this.eva = 30;
       this.def = 80;
+      this.maxHp = this.hp;
+      this.classType = "Tank";
+    }
+  }
+
+  levelUp(){
+    if (this.classType === "Fighter") {
+      this.atk += 10;
+    }else if (this.classType === "Rogue"){
+      this.eva += 10;
+    }else {
+      this.def += 10;
+    }
+    
+    return this.lvl++;
+  }
+
+  getItem(item) {
+    if (item === "pen") {
+      this.atk += 5;
+    } else if (item === "pocket-protector") {
+      this.def += 5;
+    } else if (item === "coffee") {
+      this.hp += 5;
+    } else {
+      this.eva += 5;
     }
   }
 }
@@ -34,6 +65,12 @@ let dude = new Character();
 let otherDude = new Character();
 dude.class("Fighter");
 otherDude.class("Rogue");
+
+//tests
 console.log(dude, otherDude);
+dude.hp -= 10;
 console.table(dude);
 console.table(otherDude);
+dude.levelUp();
+otherDude.levelUp();
+console.log(`after lvl up`, dude, otherDude);
